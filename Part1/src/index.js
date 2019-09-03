87ed465435de48b78a41591c6ab7fd836aa72bdf
unknown <mkoven1991@gmail.com>
2019-09-03 17:00:13 +0930

    1.5: course information as single JS object step 5 commit

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// App component - has object called course which contains some properties, including an array.
 const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
-// Variable declarations
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
  // What app returns
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts}/>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts}/>
         </div>
     )
 }
