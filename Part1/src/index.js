22948ddccd9d2e62c4e67ed99ad284115999d72e
unknown <mkoven1991@gmail.com>
2019-09-06 13:40:24 +0930

    1.7 Unicafe Step 2 complete

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -6,25 +6,29 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [poor, setPoor] = useState(0)
+    const [allFeedback, setAll] = useState([])
 
     // event handler methods
     const positiveFeedback = () => {
         console.log('Positive feedback sent.')
         setGood(good + 1)
+        setAll(allFeedback.concat(1))
     }
     const neutralFeedback = () => {
         console.log('Neutral feedback sent.')
         setNeutral(neutral + 1)
+        setAll(allFeedback.concat(0))
     }
     const negativeFeedback = () => {
         console.log('Negative feedback sent.')
         setPoor(poor + 1)
+        setAll(allFeedback.concat(-1))
     }
 
     // app returns...
     return (
         <div>
-            <h1>Unicafe: Step 1</h1>
+            <h1>Unicafe: Step 2</h1>
 
             <h2>Provide feedback:</h2>
             <button onClick={positiveFeedback}>Good</button>
@@ -34,7 +38,10 @@ const App = () => {
             <h2>Statistics</h2>
             Good: {good}<br></br>
             Neutral: {neutral}<br></br>
-            Poor: {poor}
+            Poor: {poor}<br></br>
+            All: {allFeedback.length}<br></br>
+            Average: {(good * 1 + poor * -1) / allFeedback.length} < br ></br>
+            Positive: {good / allFeedback.length * 100}%
         </div>
     )
 }
