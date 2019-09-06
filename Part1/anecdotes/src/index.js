2e888faa365697bda7278635efb851d08ac5f8c0
unknown <mkoven1991@gmail.com>
2019-09-06 17:46:21 +0930

    1.12 Anecdotes Step 1 - Random

--- Part1/anecdotes/src/index.js
+++ Part1/anecdotes/src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
     const [selected, setSelected] = useState(0)
 
+    const randomizeQuote = () => {
+        setSelected(Math.floor(Math.random() * ((anecdotes.length-1) - 0 + 1)) + 0)
+    }
+
+    console.log(anecdotes.length)
+
     return (
         <div>
-            <h1>Anecdotes</h1>
-            {props.anecdotes[selected]}
+            <h2>Random Anecdotes for Programmers</h2>
+            {props.anecdotes[selected]}<br></br>
+
+            <Button
+                method={randomizeQuote}
+                text="Randomize!"/>
         </div>
     )
 }
@@ -18,8 +28,18 @@ const anecdotes = [
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
+    'It\'s OK to figure out murder mysteries, but you shouldn\'t need to figure out code. You should be able to read it.',
+    'Program testing can be used to show the presence of bugs, but never to show their absence!',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+const Button = (props) => {
+    return (
+        <button onClick={props.method}>
+            {props.text}
+        </button>
+    )
+}
 
 ReactDOM.render(
     <App anecdotes={anecdotes} />,
