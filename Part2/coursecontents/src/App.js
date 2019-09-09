b5c75377e5a0f329c5c111ca923f285a996628a7
unknown <mkoven1991@gmail.com>
2019-09-09 15:40:40 +0930

    new folder for course content examples, before completing exercises

--- Part2/coursecontents/src/App.js
+++ Part2/coursecontents/src/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import Note from './components/Note'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = ({ notes }) => {
-    const rows = () => notes.map(note =>
-        <Note
-            key={note.id}
-            note={note}
-        />
-    )
-
-    return (
-        <div>
-            <h1>Notes</h1>
-            <ul>
-                {rows()}
-            </ul>
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
