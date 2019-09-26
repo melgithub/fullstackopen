278b1b64619f38959f065daf9fcfca9dc8fa3a75
unknown <mkoven1991@gmail.com>
2019-09-26 13:25:23 +0930

    2.4 Step 9 Added Node.js course data

--- Part2/coursecontents/src/index.js
+++ Part2/coursecontents/src/index.js
@@ -2,32 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Course from './components/course'
 
-const App = () => {
-
+const App = () =>
+{
     const courseData = [
-            { 
-            name: 'Half Stack application development',
-            id: 1,
+        { 
+        name: 'Half Stack application development',
+        id: 1,
             parts: [
-                    {
-                        name: 'Fundamentals of React',
-                        exercises: 10,
-                        id: 1
-                    },
-                    {
-                        name: 'Using props to pass data',
-                        exercises: 7,
-                        id: 2
-                    },
-                    {
-                        name: 'State of a component',
-                        exercises: 14,
-                        id: 3
-                    }
-                ]
-            }
-        ]
-
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+// Second course added
+        {
+        name: 'Node.js',
+        id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+  
 // The app component loads courses dynamically using the map function.
     return (
         <div>
