8ff6c1d85e05053a6d5f377f46a02af9a683b2b9
unknown <mkoven1991@gmail.com>
2019-09-27 13:01:01 +0930

    Exercise 2.6 Step 1 Phonebook

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,24 +1,55 @@
 import React, { useState } from 'react'
+import Person from './Components/Person'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  // array, starts empty.
+  const [ persons, setPersons] = useState([])
+  // new person name, pre-filled with a message.
+  const [ newName, setNewName ] = useState('Type your name')
 
+  // button event handler
+  const addPerson = (event) => {
+    event.preventDefault() // Stops page from refreshing when submitting
+    const personObject = { // New person object
+      content: newName,
+      id: persons.length + 1, // Temporary solution to assign ID for each person
+    }
+
+    setPersons(persons.concat(personObject)) // adds new person object to new array
+    setNewName('Type your name') // sets box back to this message after each submit
+  }
+
+  const handleNewPersonEvent = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  // Maps our persons array to our Person component
+  const entry = () => persons.map(person => 
+    <Person
+      key={person.id}
+      person={person}/>
+      )
+  
+  // What we see
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}> 
         <div>
-          name: <input />
+          Name: <input
+            value={newName}
+            onChange={handleNewPersonEvent}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button>Add</button>
         </div>
+
       </form>
-      <h2>Numbers</h2>
-      ...
+      <h3>Numbers</h3>
+      <ul>
+        {entry()}
+      </ul>
     </div>
   )
 }
