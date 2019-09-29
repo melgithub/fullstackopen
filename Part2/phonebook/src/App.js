6abfea96ae9c15a0e8511f771a4045268b5d41e5
unknown <mkoven1991@gmail.com>
2019-09-29 10:57:10 +0930

    2.9 Step 4, Phonebook Filtering

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import Person from './Components/Person'
 
 const App = () => {
-  const [ persons, setPersons] = useState([])  // array, starts empty.
+  const [ persons, setPersons] = useState([])  // array, starts empty
   const [ newName, setNewName ] = useState('')
   const [ newPhoneNumber, setNewPhoneNumber ] = useState('')
+  const [ showAll, setShowAll] = useState(true) // true by default
+  const [ filter, setFilter] = useState('') // contents of our filter
 
-  // button event handler
+  // Add button event handler
   const addPerson = (event) => {
     event.preventDefault() // Stops page from refreshing when submitting
 
@@ -27,27 +29,43 @@ const App = () => {
     setNewPhoneNumber('') 
   }
 
-  // Updates setNewName as user types in the form
+  // Updates setNewName, setPhoneNumber and setFilter as user types in the forms
   const handleNewPersonEvent = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+    setShowAll(true)
   }
   const handleNewPhoneNumberEvent = (event) => {
     console.log(event.target.value)
     setNewPhoneNumber(event.target.value)
   }
+  const handleFilterEvent = (event) => {
+    setFilter(event.target.value.toLowerCase()) // converts to lowercase for easier checking
+    setShowAll(false) // we only want to see filtered results
+  }
 
-  // Maps our persons array to our Person component
-  const entry = () => persons.map(person => 
+  // Maps our array of people to show to our Person component
+  const entry = () => peopleToShow.map(person => 
     <Person
       key={person.name}
       person={person}/>
       )
   
+  // If showAll is true, we show full list. Otherwise, we filter!
+  const peopleToShow = showAll
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().search(filter) !== -1) // if -1, no match is found.
+
   // What we see
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>React Phonebook</h2>
+      <div>
+          Filter by name: <input
+            value={filter}
+            onChange={handleFilterEvent}/>
+      </div>
+      <h3>Add new contact</h3>
       <form onSubmit={addPerson}
             value={newName}> 
         <div>
@@ -63,7 +81,6 @@ const App = () => {
         <div>
           <button>Add</button>
         </div>
-
       </form>
       <h3>Numbers</h3>
       <ul>
