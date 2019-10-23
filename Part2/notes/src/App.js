b80f4d5a73eecb763ec0f7749123aed48429e142
unknown <mkoven1991@gmail.com>
2019-10-23 11:36:42 +1030

    Part 2 Sending and Changing Server Data for Notes

--- Part2/notes/src/App.js
+++ Part2/notes/src/App.js
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
+const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
+    const toggleImportanceOf = id => {
+      const note = notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+
+      noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    }
+
     useEffect(() => {
-      console.log('effect')
-      axios
-        .get('http://localhost:3001/notes')
-        .then(response => {
-          console.log('promise fulfilled')
-          setNotes(response.data)
+      noteService
+        .getAll()
+        .then(initialNotes => {
+          setNotes(initialNotes)
         })
     }, [])
+
     console.log('render', notes.length, 'notes')
 
     const rows = () => notesToShow.map(note =>
       <Note
         key={note.id}
         note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)}
       />
     )
     
@@ -35,10 +52,11 @@ const App = (props) => {
         date: new Date().toISOString(),
         important: Math.random() > 0.5
       }
-      axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+
+      noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
     }
