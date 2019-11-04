8ad14af20964c2c246ceba1d3fffac902e7a6f11
unknown <mkoven1991@gmail.com>
2019-11-04 11:25:18 +1030

    "Reverting to 401d11b change"
    This reverts commit 3c9a3d791749f3c5e0d01e0b7c08b5216a7add27.

---
+++ Part3/notesbackend/build/precache-manifest.2db79eaf514585e9755b4f40f931ac6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217d328bd91d4d828158433dc87b9f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421b50fabfa6420593af",
+    "url": "/static/css/main.b53e853b.chunk.css"
+  },
+  {
+    "revision": "459da4f16745ece314c4",
+    "url": "/static/js/2.8ba273de.chunk.js"
+  },
+  {
+    "revision": "421b50fabfa6420593af",
+    "url": "/static/js/main.c2617828.chunk.js"
+  },
+  {
+    "revision": "7dd24b8cd31e7e6d9946",
+    "url": "/static/js/runtime~main.45f5d8a8.js"
+  }
+]);
---
+++ Part3/phonebookbackend/README.md
@@ -0,0 +1,2 @@
+Index: https://nameless-mountain-95639.herokuapp.com/
+People API: https://nameless-mountain-95639.herokuapp.com/api/persons
