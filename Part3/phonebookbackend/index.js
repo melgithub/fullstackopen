efd668a83baa23e174292bbe45131336f634296f
unknown <mkoven1991@gmail.com>
2019-11-04 12:00:12 +1030

    Completed 3.14, step 2. Can add to database from phonebook

--- Part3/phonebookbackend/index.js
+++ Part3/phonebookbackend/index.js
@@ -41,18 +41,10 @@ let persons = [
     ]
 
     // ---- HTTP GET REQUESTS
-    // app.get('/', (req, res) => {
-    //     res.send('<h1>3.10: Phonebook backend, Step 10</h1>')
-    // })
-
     app.get('/info', (req, res) => {
         const date = new Date();
         res.send(`Phonebook contains info for ${persons.length} people.<br><br>${date}`)
     })
-
-    // app.get('/api/persons', (req, res) => {
-    //     res.json(persons)
-    // })
 
     app.get('/api/persons', (req, res) => {
         Person.find({}).then(persons => {
@@ -60,26 +52,13 @@ let persons = [
         })
     })
 
-    app.get('/api/persons/:id', (req, res) => {
-        const id = Number(req.params.id) // If we don't cast as Number, we get a var mismatch and won't get the person
-        const person = persons.find(person => person.id === id)
-
-        if (person) { // If object is truthy (not null/undef)
-            res.json(person)
-        }
-        else {
-            res.status(404).end()
-        }
-    })
+    app.get('/api/persons/:id', (request, response) => {
+        Person.findById(request.params.id).then(person => {
+          response.json(person.toJSON())
+        })
+      })
 
     // ---- HTTP POST REQUEST
-    const generateID = () => {
-        const maxId = persons.length > 0
-          ? Math.max(...persons.map(n => n.id))
-          : 0
-        return maxId + 1
-      }
-      
     app.post('/api/persons', (req, res) => {
         const body = req.body
         
@@ -93,23 +72,19 @@ let persons = [
                 error: 'Number missing!'
             })
         }
-        else if (persons.find(p => p.name === body.name)){
-            return res.status(400).json({
-                error: `${body.name} already in phonebook.`
-            })
-        }
 
-        const person = { // constructor
+        const person = new Person ({ // constructor
             name: body.name,
             number: body.number,
-            id: generateID(),
-        }
+        })
 
-        persons = persons.concat(person)
-        res.json(person)
+        person.save().then(savedPerson => {
+            res.json(savedPerson.toJSON())
+        })
     })
 
     // ---- HTTP DELETE REQUESTS
+    // Not yet functional with database
     app.delete('/api/persons/:id', (req, res) => {
         const id = Number(req.params.id)
         persons = persons.filter(person => person.id !== id)
