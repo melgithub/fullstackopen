ba3b680f57e7c4b90cb57813689d2381a0a58e2b
unknown <mkoven1991@gmail.com>
2019-11-07 11:24:09 +1030

    Pre part4, section 1 commit. Added testing examples in Notes app

---
+++ Part3/notesbackend/tests/palindrome.test.js
@@ -0,0 +1,30 @@
+// Individual test cases are defined with the test function.
+// The first parameter of the function is the test description as a string.
+// The second parameter is a function that defines the functionality for the test case.
+
+const palindrome = require('../utils/for_testing').palindrome
+
+test('palindrome of a', () => {
+  const result = palindrome('a')
+
+  expect(result).toBe('a')
+})
+
+test('palindrome of react', () => {
+  const result = palindrome('react')
+
+  expect(result).toBe('tcaer')
+})
+
+test('palindrome of releveler', () => {
+  const result = palindrome('releveler')
+
+  expect(result).toBe('releveler')
+})
+
+// // This one fails testing
+// test('palindrome of react', () => {
+//   const result = palindrome('react')
+
+//   expect(result).toBe('tkaer')
+// })
