1ea82e9d673a2b6f7cef1826a74c99e84c2db30f
unknown <mkoven1991@gmail.com>
2019-11-12 15:38:47 +1030

    4.10, step 3 DB gets fresh info whenever we run tests. Added POST new blog test, working.

--- Part4/bloglist/tests/blog_api.test.js
+++ Part4/bloglist/tests/blog_api.test.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 describe('Blog Tests', () => {
   test('testing content type is returned as JSON', async () => {
@@ -13,16 +23,38 @@ describe('Blog Tests', () => {
 
   test('length of entries returned', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(2)
+    expect(response.body.length).toBe(3)
   })
 
-  test('ID', async () => {
+  test('IDs are defined', async () => {
     const response = await api.get('/api/blogs')
     const identifier = response.body.map(r => r.id)
     //expect(identifier[0]).toBeDefined()
     for (let i = 0; i < identifier.length; i++) {
       expect(identifier[i]).toBeDefined()
     }
+  })
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'Generic Test Vlog',
+      author:'Barbie',
+      url: 'https://en.wikipedia.org/',
+      likes: 99
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    // Confirm length is right
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+    // Confirm new title data is in db entry
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(newBlog.title)
   })
 })
 
---
+++ Part4/bloglist/tests/test_helper.js
@@ -0,0 +1,30 @@
+const Blog = require('../models/blog')
+
+// Data to reset our testing DB when we run tests
+const initialBlogs = [
+  {
+    title: 'Magical Blog',
+    author:'Matilda Wormwood',
+    url: 'https://en.wikipedia.org/wiki/Matilda_(1996_film)',
+    likes: 56  },
+  {
+    title: 'Testers Blog ',
+    author:'Matilda Honey',
+    url: 'https://en.wikipedia.org/wiki/Matilda_(1996_film)',
+    likes: 256  },
+  {
+    title: 'Ultimate Guide to Japan',
+    author:'Katie Diederichs',
+    url: 'https://www.twowanderingsoles.com/blog/ultimate-japan-travel-guide-everything-you-need-to-know-for-your-first-visit',
+    likes: 353  },
+]
+
+// Gets the database blogs as JSON
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, blogsInDb
+}
