97fac181458e1a6a2cb420b5f6a2cb9076aa660a
unknown <mkoven1991@gmail.com>
2019-11-17 10:52:26 +1030

    Token auth implementation

--- Part4/notes/app.js
+++ Part4/notes/app.js
@@ -3,6 +3,7 @@ const express = require('express') // dictates that we need express
 const bodyParser = require('body-parser')
 const app = express() // creates an express app
 const cors = require('cors') // middleware to allow requests from other origins
+const loginRouter = require('./controllers/login')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
@@ -26,6 +27,7 @@ app.use(cors())
 app.use(express.static('build')) //middleware to serve static files such as images, CSS files, and JS files
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
---
+++ Part4/notes/controllers/login.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  // searches for user based on username attached to the request
+  const user = await User.findOne({ username: body.username })
+  // checks the password attached to the req using bcryp.compare since passes are not in db
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) { 
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+  // if pw is correct, token is created with jwt.sign!
+  // it is digitally signed using SECRET env variable
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  // if req is a success
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
