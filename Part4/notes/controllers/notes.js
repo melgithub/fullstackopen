bdc59e9cd7c42b189ecd6a6b84bf3611d0f7423a
unknown <mkoven1991@gmail.com>
2019-11-17 17:03:46 +1030

    Using token auth to POST notes

--- Part4/notes/controllers/notes.js
+++ Part4/notes/controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -24,21 +33,29 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
 
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    date: new Date(),
-    user: user._id
-  })
   try {
-    const savedNote = await note.save() // save the new Note
-    user.notes = user.notes.concat(savedNote._id) // add the saved note to the relevant User
-    await user.save() // save the relevant User
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({
+        error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id
+    })
+
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
     response.json(savedNote.toJSON())
-  }
-  catch (exception) {
+  } catch(exception) {
     next(exception)
   }
 })
